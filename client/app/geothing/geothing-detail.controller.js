edbe3569eb13715ff803d95e4533325d46905ef9
Leigh Hunt <leigh@venari.co.nz>
2015-08-14 22:26:04 +1200

    Not displaying correcty yet

--- client/app/geothing/geothing-detail.controller.js
+++ client/app/geothing/geothing-detail.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('geoTestApp')
-  .controller('GeothingDetailCtrl', function ($scope) {
+  .controller('GeothingDetailCtrl', function ($scope, $http, $stateParams) {
     $scope.message = 'Hello from the Detail Controller';
+
+    $scope.id = $stateParams.id;
+    $scope.geothing = {};
+
+		$http.get('/api/geothings/' + $scope.id).success(function(geothing) {
+			$scope.geothing = geothing;
+
+		}).
+    error(function(e) {
+      handleError(e);
+    });
   });
